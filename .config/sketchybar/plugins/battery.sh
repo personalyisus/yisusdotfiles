03681e68652d1a7d70f97e5d67418846e871998d
Jesús Marín <marinalvjesus@gmail.com>
2025-02-20 00:18:56 -0300

    feat: added some color to the energy icon and made dates nicer for me

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -8,6 +8,17 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
+  9[0-9]|100) COLOR="0xff32d609"
+  ;;
+  [6-8][0-9]) COLOR="0xff32d609"
+  ;;
+  [3-5][0-9]) COLOR="0xfff5e616"
+  ;;
+  [1-2][0-9]) COLOR="0xffe87509"
+  ;;
+  *) ICON="0xfffc1100"
+esac
+case "${PERCENTAGE}" in
   9[0-9]|100) ICON=""
   ;;
   [6-8][0-9]) ICON=""
@@ -21,8 +32,9 @@ esac
 
 if [[ "$CHARGING" != "" ]]; then
   ICON=""
+  COLOR="0xff32d609" # green hex color
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" icon.color="$COLOR"
