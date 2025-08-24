28cf5d3985dba09e4d064176da30087fbfb3ef0d
Jesús Marín <marinalvjesus@gmail.com>
2025-08-23 20:03:56 -0400

    chore: small tweaks to the bar

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -4,7 +4,9 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=true
+    # sketchybar --set $NAME background.drawing=true
+    sketchybar --animate linear 10 --set $NAME background.color=0x39ffffff
 else
-    sketchybar --set $NAME background.drawing=false
+    sketchybar --animate linear 10 --set $NAME background.color=0x00000000
+    # sketchybar --set $NAME background.drawing=false
 fi
